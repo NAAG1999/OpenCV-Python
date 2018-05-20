057eb24073ef01ac4371b4b94c9f275c8e3c38b9
Nilesh <agnilesh8199@gmail.com>
2018-05-20 17:42:34 +0530

    drawing-writing

---
+++ drawWrite_image/draw_write.py
@@ -0,0 +1,29 @@
+import numpy as np
+import cv2
+
+img = cv2.imread('/home/nilesh/Desktop/Work/OpenCV/loadingImages/watch.jpg',cv2.IMREAD_COLOR)
+
+cv2.line(img, (0,0), (150,150), (255,0,0), 15)
+              #coordinates       #B  G  R  #width
+cv2.rectangle(img, (15,25), (156,75), (255,255,255),  5)
+
+cv2.circle(img, (156,75), 55, (0,0,255), )
+
+#for drawing a polygon, We do as :-
+
+pts = np.array([[10,5], [20,30], [70,30], [50,10]], np.int32)
+#pts = pts.reshape((-1,1,2)) 
+
+cv2.polylines(img, [pts], True, (0,255,255), 1)
+
+
+#write
+
+font =cv2.FONT_HERSHEY_SIMPLEX
+cv2.putText(img, 'OpenCV !',(0,130), font, 1, (200,255,255),2, cv2.LINE_AA)
+
+cv2.imshow('image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()              
+
+#for arguments read documentation
